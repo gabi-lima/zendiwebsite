66f560da4c5b8b06f9a94d127bc07850bf9098e0
Gabriel Lima <gabriel1277lima@gmail.com>
2023-08-17 08:14:56 -0300

    star rating

--- js/modal.js
+++ js/modal.js
@@ -2,3 +2,36 @@ function OpenModal() {
   modal = document.querySelector("#modal");
   modal.showModal();
 }
+
+const stars = document.querySelectorAll(".stars a");
+const stars2 = document.querySelectorAll(".stars2 a");
+const stars3 = document.querySelectorAll(".stars3 a");
+
+stars.forEach((star, index1) => {
+  star.addEventListener("click", () => {
+    stars.forEach((star, index2) => {
+      index1 >= index2
+        ? star.classList.add("text-yellow-500")
+        : star.classList.remove("text-yellow-500");
+    });
+  });
+});
+
+stars2.forEach((star2, index1) => {
+  star2.addEventListener("click", () => {
+    stars2.forEach((star2, index2) => {
+      index1 >= index2
+        ? star2.classList.add("text-yellow-500")
+        : star2.classList.remove("text-yellow-500");
+    });
+  });
+});
+stars3.forEach((star3, index1) => {
+  star3.addEventListener("click", () => {
+    stars3.forEach((star3, index2) => {
+      index1 >= index2
+        ? star3.classList.add("text-yellow-500")
+        : star3.classList.remove("text-yellow-500");
+    });
+  });
+});
