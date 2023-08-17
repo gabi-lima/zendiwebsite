170c729f54588b842794592fb9f96a3cbd62655d
Gabriel Lima <gabriel1277lima@gmail.com>
2023-08-17 04:23:31 -0300

    .

--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,13 @@
 function Open() {
-  document.getElementById("sidebar").classList.remove("left-[-300px]");
-  document.querySelector("#body").classList.toggle("hidden");
+  //   document.getElementById("sidebar").classList.remove("left-[-300px]");
+  document.querySelector("#bodyhide").classList.toggle("hidden");
+  document.querySelector("#sidebar").classList.toggle("hidden");
 
   document.querySelector("#nav").classList.toggle("hidden");
 }
 function Close() {
-  document.getElementById("sidebar").classList.add("left-[-300px]");
+  //   document.getElementById("sidebar").classList.add("left-[-300px]");
   document.querySelector("#nav").classList.toggle("hidden");
-  document.querySelector("#body").classList.toggle("hidden");
+  document.querySelector("#bodyhide").classList.toggle("hidden");
+  document.querySelector("#sidebar").classList.toggle("hidden");
 }
